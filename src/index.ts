c466e66f09f2b3b51174d2d97b3f882b3487b871
缪宇 <miaoyu@sefonsoft.com>
2020-04-17 14:44:25 +0800

    config webpack with typescript

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,10 @@
  * @Author: miaoyu
  * @Date: 2020-04-17 13:40:42
  * @LastEditors: miaoyu
- * @LastEditTime: 2020-04-17 13:50:23
+ * @LastEditTime: 2020-04-17 14:43:32
  * @Description:
  */
-export const Greeter = (name: string) => `Hello ${name}`;
+const md = {
+  greeter:(name: string) => `Hello ${name} `
+}
+export default md
--- webpack.config.js
+++ webpack.config.js
@@ -2,16 +2,28 @@
  * @Author: miaoyu
  * @Date: 2020-04-17 13:42:03
  * @LastEditors: miaoyu
- * @LastEditTime: 2020-04-17 13:44:29
+ * @LastEditTime: 2020-04-17 14:42:25
  * @Description: 
  */
 const path = require('path');
 
 module.exports = {
-  mode: "production",
-  entry: './src/index.js',
+  // mode: "production",
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ],
+  },
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'lib'),
   },
 };
